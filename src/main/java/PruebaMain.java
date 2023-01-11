87843aaf1f85d07686289e0db9c9435c2b0deb13
xrosilloprofe <xv.rosillo@edu.gva.es>
2023-01-11 20:09:01 +0100

    initial commit

--- src/main/java/PruebaMain.java
+++ src/main/java/PruebaMain.java
@@ -6,9 +6,9 @@ public class PruebaMain {
         Coordinate c1 = new Coordinate('G',1);
         Board tablero = new Board();
         Cell celda = new Cell(tablero, c1);
-        Piece caballoBlanco = new Piece(Piece.Type.WHITE_KING,celda);
+        Piece caballoBlanco = new Piece(Piece.Type.WHITE_KNIGHT,celda);
         System.out.println(caballoBlanco);
-        Piece caballoNegro = new Piece(Piece.Type.BLACK_KING,celda);
+        Piece caballoNegro = new Piece(Piece.Type.BLACK_KNIGHT,celda);
         System.out.println(caballoNegro);
     }
 
